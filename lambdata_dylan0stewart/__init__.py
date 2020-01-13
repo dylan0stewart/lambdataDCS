bec7aa91410d1032f7ddad1e4cf34168e68d1195
dylan0stewart <dylan0stewart@users.noreply.github.com>
2020-01-13 17:57:10 -0500

    added init/util files

---
+++ lambdata_dylan0stewart/__init__.py
@@ -0,0 +1,8 @@
+"""
+lambdata - a collection of data science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50)) 
---
+++ lambdata_dylan0stewart/df_utils.py
@@ -0,0 +1,5 @@
+"""
+Utility functions for working with DataFrames
+"""
+import pandas
+TEST_DF = pandas.DataFrame([1,2,3])
