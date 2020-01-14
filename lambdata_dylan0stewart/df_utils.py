faa14ceb8a6ec5f14a69d9a510a81b5a35a5a7b4
dylan0stewart <dylan0stewart@users.noreply.github.com>
2020-01-14 15:55:49 -0500

    added functions

--- lambdata_dylan0stewart/df_utils.py
+++ lambdata_dylan0stewart/df_utils.py
@@ -2,4 +2,21 @@
 Utility functions for working with DataFrames
 """
 import pandas
-TEST_DF = pandas.DataFrame([1,2,3])
+
+def tvt_split(X):
+    '''
+    This function will split your dataframe into a 60%/20%/20% split for your
+    train, validation, and test sets accordingly.
+    '''
+    X = X.copy()
+    train, validate, test = np.split(.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+
+def show_nans(X):
+    '''
+    This Function will copy your dataframe, then print out a list of which 
+    columns have NaNs, and how many there are in that column.
+    '''
+    X = X.copy()
+    cols_w_nans = X.columns[X.isna().any()].tolist()
+    df_nan_cols = pd.DataFrame(df[cols_with_nans])
+    return df_nan_cols.isnull().sum()
--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ REQUIRED = [
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
-        name="lambdata-DS10",
-        version= "0.1.1",
+        name="lambdata-dylan0stewart",
+        version= "0.1.4",
         author= "dylan0stewart",
         description= " a collection of data science helper functions",
         long_description= LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
-        url="https://lambdaschool.com/courses/data-science",
+        url="https://github.com/dylan0stewart/lambdataDCS",
         packages=setuptools.find_packages(),
         python_requires=">=3.5",
         install_requires= REQUIRED,
